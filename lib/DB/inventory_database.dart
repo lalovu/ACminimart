132aa9597499c0861c2c3c0aa71f956ebb706da2
Junyl JB Gonzales <junyljbgonzales@Junyls-MacBook-Pro.local>
2024-03-09 19:37:34 +0800

    Updated

--- lib/DB/inventory_database.dart
+++ lib/DB/inventory_database.dart
@@ -1,3 +1,4 @@
+import 'package:dbase/Model/products.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:dbase/widget/main.dart';
@@ -26,8 +27,82 @@ class ACDatabase {
   }
 
   Future _createDB(Database db, int version) async{
+    final idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    final textType = 'TEXT NOT NULL';
+    final integerType = 'INTEGER NOT NULL';
+    final doubleType = 'DOUBLE NOT NULL';
+
+
+
+
+    await db.execute('''
+CREATE TABLE $tableInventory (
+${ProductFields.id},$idType,
+${ProductFields.name},$textType,
+${ProductFields.description},$textType,
+${ProductFields.category},$textType,
+${ProductFields.quantity},$integerType,
+${ProductFields.price},$doubleType,
+)
+
+''');
 
   }
+  Future<Products> create(Products product) async {
+    final db = await instance.database;
+
+    final id = await db.insert(tableInventory, product.toJson());
+    return product.copy(id:id);
+  }
+
+  Future<Products> checkProduct(String category) async {
+  final db = await instance.database;
+
+  List<Map<String, dynamic>> maps;
+
+  if (category.toUpperCase() == 'ALL') {
+    maps = await db.query(
+      tableInventory,
+      columns: ProductFields.values,
+    );
+  } else {
+    maps = await db.query(
+      tableInventory,
+      columns: ProductFields.values,
+      where: '${ProductFields.category} = ?',
+      whereArgs: [category],
+    );
+  }
+
+   if (maps.isNotEmpty) {
+    return Products.fromJson(maps.first);
+  } else {
+    throw Exception('No products found for category: $category');
+  }
+
+  }
+
+  Future <int> update(Products product) async {
+    final db = await instance.database;
+
+    return db.update(
+      tableInventory,
+      product.toJson(),
+      where: '${ProductFields.id} = ?',
+      whereArgs: [product.id],
+    ); 
+  }
+
+  Future<int> delete (int id) async {
+    final db = await instance.database;
+
+    return await db.delete(
+      tableInventory,
+      where: '${ProductFields.id} = ?',
+      whereArgs: [id],
+    );
+  }
+
 
   Future close() async {
     final db = await instance.database;
@@ -35,6 +110,7 @@ class ACDatabase {
     db.close();
 
   }
+  
 }
 
 
--- lib/Model/products.dart
+++ lib/Model/products.dart
@@ -1,6 +1,11 @@
 final String tableInventory = 'Products';
 
 class ProductFields {
+
+  static final List <String> values = [
+    id, name, description, category, quantity, price
+  ];
+
   static final String id = '_id';
   static final String name = 'name';
   static final String description = 'description';
@@ -12,11 +17,11 @@ class ProductFields {
 
 class Products {
   final int? id;
-  final String? name;
-  final String? description;
-  final String? category;
-  final int? quantity;
-  final double? price;
+  final String name;
+  final String description;
+  final String category;
+  final int quantity;
+  final double price;
 
   const Products ({
     this.id,
@@ -26,4 +31,39 @@ class Products {
     required this.quantity,
     required this.price,
   });
+
+  Products copy({
+    int? id,
+    String? name,
+    String? description,
+    String? category,
+    int? quantity,
+    double? price,
+  }) =>
+      Products(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        description: description ?? this.description,
+        category: category ?? this.category, 
+        quantity: quantity ?? this.quantity,
+        price: price ?? this.price,
+      );
+      
+  static Products fromJson(Map<String, Object?> json) => Products(
+    id: json[ProductFields.id] as int?,
+    name: json[ProductFields.name] as String,
+    description: json[ProductFields.description] as String,
+    category: json[ProductFields.category] as String,
+    quantity: json[ProductFields.quantity] as int,
+    price: json[ProductFields.price] as double,
+  );
+
+  Map<String, Object?> toJson() => {
+    ProductFields.id:id,
+    ProductFields.name:name,
+    ProductFields.description: description,
+    ProductFields.category: category,
+    ProductFields.quantity: quantity,
+    ProductFields.price: price,
+  };
 }
